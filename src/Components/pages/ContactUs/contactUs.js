45eead8127a9a23d14d49c02f29c18b10797c48f
AnoopAG91 <anoopag47@gmail.com>
2023-07-16 09:51:02 +0530

    new gym branch

--- src/Components/pages/ContactUs/contactUs.js
+++ src/Components/pages/ContactUs/contactUs.js
@@ -16,9 +16,8 @@ function ContactUs() {
        <div className={Styles.ContactUs_Container}>
 
           <div className={Styles.Map}>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242118.17006018994!2d73.69815394055104!3d18.52454503977253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2e67461101%3A0x828d43bf9d9ee343!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1689171256442!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
-
-          </iframe>
+          <iframe class="gmap_iframe" width="100%" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=kozhikode&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" style={{width:400, height:600}}></iframe>
+           
           </div>
           <div className={Styles.Form}>
             <form className={Styles.Form} 
